ccc691b46171493fd454f8de70df28ea03d626e1
Kensuke <kensuke.mit@gmail.com>
2025-05-25 12:48:10 +0200

    updating visualisation configs

--- README.md
+++ README.md
@@ -5,6 +5,16 @@
 1. Setting a word embedding file (numpy) and corresponding picle file of token entries. See the next section for the details.
 2. Running `./word_sense_change_analysis/preprocessing.py` with a config file.
 3. Running `./interface.py`
+4. Running `./word_sense_change_analysis/visualisations.py`
+
+### Visualisation
+
+
+```
+python ./word_sense_change_analysis/visualisations.py -c <path-to-config.toml>
+```
+
+
 
 
 # Required File Format
--- word_sense_change_analysis/visualisations.py
+++ word_sense_change_analysis/visualisations.py
@@ -48,27 +48,7 @@ except ImportError as e:
 # -------------------------------------------------
 # matplotlib configs
 import matplotlib.pyplot as plt
-import matplotlib
-from matplotlib.font_manager import FontProperties
-
-SMALL_SIZE = 8
-MEDIUM_SIZE = 10
-BIGGER_SIZE = 20
-
-BIG_TITLE_SIZE = 20
-
-font = {'size'   : 22}
-
-matplotlib.rc('font', **font)
-matplotlib.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
-matplotlib.rc('axes', labelsize=BIGGER_SIZE)    # fontsize of the x and y labels
-matplotlib.rc('xtick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
-matplotlib.rc('ytick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
-matplotlib.rc('legend', fontsize=BIGGER_SIZE)    # legend fontsize
-matplotlib.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
-
-matplotlib.rcParams['text.usetex'] = False
-# -------------------------------------------------
+from word_sense_change_analysis import visualisation_header
 
 
 # TimeEpochLabelStart = 2003
